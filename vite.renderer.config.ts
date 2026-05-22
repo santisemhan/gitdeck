b3146618c8004a8e76fe85dc663c1ebfd8718c29
Santiago Semhan <58712215+santisemhan@users.noreply.github.com>
2026-05-21 22:14:57 -0300

    fix: electron base path (#6)

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -4,6 +4,7 @@ import path from "node:path";
 
 export default defineConfig({
   root: path.resolve(__dirname, "src/renderer"),
+  base: "./",
   plugins: [react()],
   build: {
     outDir: path.resolve(__dirname, "dist/renderer"),
